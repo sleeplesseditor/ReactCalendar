0b28fcedfc58883252aa02927de4bb323604c311
sleeplesseditor <josh@bryson.me.uk>
2018-06-18 00:05:20 +0100

    Month change function setup

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Calendar from './Calendar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1 className="App-title">React Calendar</h1>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Calendar.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import dateFns, { startOfWeek } from 'date-fns';
+
+class Calendar extends Component {
+    state = {
+        currentMonth: new Date(),
+        selectedDate: new Date()
+    };
+
+    renderHeader() {
+        const dateFormat = "MMMM YYYY";
+
+        return (
+            <div className="header row flex-middle">
+                <div className="col col-start">
+                    <div className="icon" onClick={this.prevMonth}>
+                        chevron_left
+                    </div>
+                </div>
+                <div className="col col-center">
+                    <span>
+                        {dateFns.format(this.state.currentMonth, dateFormat)}
+                    </span>
+                </div>
+                <div className="col col-end" onClick={this.nextMonth}>
+                    <div className="icon">
+                        chevron_right
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
+    renderDays() {
+        const dateFormat = "dddd";
+        const days = [];
+
+        let startDate = dateFns/startOfWeek(this.state.currentMonth);
+
+        for (let i = 0; i < 7; i++) {
+            days.push(
+                <div className="col col-center" key={i}>
+                    {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+                </div>
+            );
+        }
+
+        return <div className="days row">{days}</div>;
+    }
+
+    renderCells() {
+        const { currentMonth, selectedDate } = this.state;
+        const monthStart = dateFns.startOfMonth(currentMonth);
+        const monthEnd = dateFns.endOfMonth(monthStart);
+        const startDate = dateFns.startOfWeek(monthStart);
+        const endDate = dateFns.endOfWeek(monthEnd);
+
+        const dateFormat = "D";
+        const rows = [];
+
+        let days = [];
+        let day = startDate;
+        let formattedDate = "";
+
+
+    }
+
+    onDateClick = day => {
+        this.setState({
+            selectedDate: day
+        });
+    };
+
+    nextMonth = () => {
+        this.setState({
+            currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+        });
+    };
+
+    prevMonth = () => {
+        this.setState({
+            currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+        });
+    };
+
+    render() {
+        return (
+            <div className="calendar">
+                {this.renderHeader()}
+                {this.renderDays()}
+                {this.renderCells()}
+            </div>
+        );
+    }
+}
+
+export default Calendar;
